3dd809c2c3eb57618b5b335a3bee80cebd52ac59
Emily Tran <trane@oxy.edu>
2018-10-24 23:33:25 -0700

    testing

--- oxycsbot.py
+++ oxycsbot.py
@@ -285,8 +285,21 @@ class OxyCSBot(ChatBot):
     # "waiting" state functions
 
     def respond_from_waiting(self, message, tags):
+        self.professor = None
+
         if "sad" in tags:
             return self.go_to_state('why_sad')
+        else:
+            return self.finish('checkpoint')
+
+        if 'office-hours' in tags:
+            for professor in self.PROFESSORS:
+                if professor in tags:
+                    self.professor = professor
+                    return self.go_to_state('specific_faculty')
+            return self.go_to_state('unknown_faculty')
+        elif 'thanks' in tags:
+            return self.finish('thanks')
         else:
             return self.finish('confused')
 
@@ -303,11 +316,10 @@ class OxyCSBot(ChatBot):
 
     def respond_from_why_sad(self, message, tags):
         if "failing academics" in tags:
-            return self.finish('checkpoint')
-            #return self.go_to_state('talk_to_professors')
+            return self.go_to_state('talk_to_professors')
 
 
-
+    #
 
 
 
