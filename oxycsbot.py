c8263f0aea428b06dc6bbda40e5ac03fddfea750
Emily Tran <trane@oxy.edu>
2018-10-26 03:43:48 -0700

    commented out pottential cause

--- oxycsbot.py
+++ oxycsbot.py
@@ -336,7 +336,7 @@ class OxyCSBot(ChatBot):
 
     def on_enter_anxious_breathe(self):
         print("Let's take some time to bring ourselves back to the present. Inhale with me")
-        self.breatheCountdown(10)
+        # self.breatheCountdown(10)
         return "Do you feel better?"
 
     def respond_from_anxious_breathe(self, message, tags):
